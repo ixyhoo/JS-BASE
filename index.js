8d1532ea3afde3ee896446f92b32e50e0e55c12c
ixyhoo <ixyhoo@outlook.fr>
2023-04-18 14:44:06 +0200

    variable and const

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-alert("hello guys");
+//alert("hello guys");
+
+console.log("coucou");
+
+
+let maSuperVariable = "hello";
+
+var unText = "voici un texte";
+console.log(unText);
+
+const Prenom = "ade";
+
+console.log(Prenom);
+
+let number = 4
+
+console.log(number);
