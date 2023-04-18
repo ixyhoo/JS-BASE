906276a3cbc8280a01bbedf7a9c5898949249513
ixyhoo <ixyhoo@outlook.fr>
2023-04-18 14:51:10 +0200

    concatenation

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 
 console.log("coucou");
 
-
-let maSuperVariable = "hello";
+let maSuperVariable = "hello";
 
 var unText = "voici un texte";
 console.log(unText);
@@ -12,6 +11,12 @@ const Prenom = "ade";
 
 console.log(Prenom);
 
-let number = 4
+let number = 4;
 
 console.log(number);
+
+let string = "I'am a sting";
+
+let newString =" for Javascript"
+
+console.log(string  +  newString);
