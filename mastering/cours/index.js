88160f5e72add97b7ba8072717ae1096a4032cd4
ixyhoo <ixyhoo@outlook.fr>
2023-04-22 14:08:33 +0200

    selector Js

--- mastering/cours/index.js
+++ mastering/cours/index.js
@@ -1,1 +1,13 @@
-document.querySelector("")
+//document.querySelector("h4").style.color="green";
+
+/*const baliseHtml = document.querySelector("h4");
+
+console.log(baliseHtml);
+
+baliseHtml.style.color="green";*/
+
+//Click event
+
+const qContainer = document.querySelector(".click-event");
+
+qContainer.addEventListener("")
