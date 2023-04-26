1128cd7c2628ecdc3d50ead8bb3797ba71d1f1e9
ixyhoo <ixyhoo@outlook.fr>
2023-04-26 14:14:58 +0200

    game

--- mastering/cours/index.js
+++ mastering/cours/index.js
@@ -9,7 +9,22 @@ baliseHtml.style.color="green";*/
 //Click event
 
 const qContainer = document.querySelector(".click-event");
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.getElementById("btn-2");
+const reponse = document.querySelector("p");
 
 qContainer.addEventListener("click", ()=>{
  qContainer.classList.toggle("question-click")
 });
+
+btn1.addEventListener("click", () => {
+    reponse.style.visibility = "visible";
+    reponse.style.background = "green";
+})
+btn2.addEventListener("click", () => {
+    reponse.style.visibility = "visible";
+    reponse.style.background = "red";
+})
+
+
+//----------------------------------------------------------------
