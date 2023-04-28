224a9f4fbb4d88c57d4efb5d5234c9bdbf257319
ixyhoo <ixyhoo@outlook.fr>
2023-04-28 14:26:47 +0200

    form & input

--- mastering/cours/index.js
+++ mastering/cours/index.js
@@ -89,4 +89,16 @@ document.addEventListener('keypress', (e) => {
    ring();
 });
 
+//---
 
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 120) {
+        nav.style.top = 0;
+    } else {
+        nav.style.top = "-50px";
+    }
+})
+
+//---
