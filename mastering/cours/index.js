d78456eeeec41c7616976e943bcc60ffbab87bc9
ixyhoo <ixyhoo@outlook.fr>
2023-04-28 09:54:47 +0200

    mousedown

--- mastering/cours/index.js
+++ mastering/cours/index.js
@@ -36,4 +36,14 @@ const mousemove = document.querySelector(".mousemove");
 window.addEventListener("mousemove", (e) => {
    mousemove.style.left = e.pageX + "px";
    mousemove.style.top = e.pageY + "px";
+});
+
+window.addEventListener("mousedown", () => {
+    mousemove.style.transform = "scale(2) translate(-25%, -25%)";
+    mousemove.style.border = "3px solid yellow";
+})
+
+window.addEventListener("mouseup", () => {
+    mousemove.style.transform = "scale(1) translate(-50%, -50%)";
+    mousemove.style.border = "2px solid red";
 })
