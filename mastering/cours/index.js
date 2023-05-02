3426f88105b63f223cb1797b2334451f997d6d74
ixyhoo <ixyhoo@outlook.fr>
2023-05-02 09:55:40 +0200

    event load

--- mastering/cours/index.js
+++ mastering/cours/index.js
@@ -104,8 +104,29 @@ window.addEventListener("scroll", () => {
 //---
 
 const inputName = document.querySelector("input[type='text']");
-let pseudo = "";
+const select = document.querySelector("select")
+const form = document.querySelector("form");
+let pseudo = ""
+let language = ""
+
 
 inputName.addEventListener("input", (e) => {
     pseudo = e.target.value;
-});
+})
+
+select.addEventListener("input", (e) => {
+    language = e.target.value;
+})
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (cgv.checked) {
+        document.querySelector("form > div").innerHTML = `
+        <h3>Pseudo: ${pseudo}</h3>
+        <h4>Language: ${language}</h4>
+        `;
+    } else {
+        alert("Veuillez accepter les CGV");
+    }
+})
